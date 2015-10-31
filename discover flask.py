1c9be4fb06a2e81c5dc1d14c84dc18c4b26c31be
Sarkis Derderian <sarkisderderian@hotmail.com>
2015-10-31 13:15:06 +0400

    none

--- discover flask.py
+++ discover flask.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask import render_template
 from flask import redirect,request,url_for
+from flask import session
 
 app = Flask(__name__)
+app.secret_key="my precious"
 
 
 @app.route('/')
@@ -25,9 +27,14 @@ def login():
         if request.form['username']!= 'admin' or request.form['password'] != 'admin':
             error='Invalid credentials, Please try agein.'
         else:
+            session['logged_in']=True
             return redirect(url_for('home'))
     return render_template('login.html',error=error)
 
+@app.route('/logout')
+def logout():
+    session.pop('logged_in',None)
+    return redirect(url_for('welcome'))
 
 if __name__ == '__main__':
     app.run(port=3030,debug=True)
